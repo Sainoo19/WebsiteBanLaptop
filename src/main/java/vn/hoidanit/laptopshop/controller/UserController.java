1e780b59e8310864708787a1c7468273ca01b096
Sainoo <nvtrung19.work@gmail.com>
2024-05-17 11:29:36 +0700

    code view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,9 +1,8 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -17,7 +16,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        return "TestRender.html";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("object1", test);
+        model.addAttribute("object2", "Hello from controller");
+        return "hello";
     }
 }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     public String handleHello() {
-        return "Hello by Sainoo !!";
+        return "Hello by Sainoo from service";
     }
 }
