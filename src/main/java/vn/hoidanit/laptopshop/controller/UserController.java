76bc9165f56239dc13106c6a7f7c356ddb92b69d
Sainoo <nvtrung19.work@gmail.com>
2024-05-17 15:28:42 +0700

    ceate form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,8 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -20,6 +23,18 @@ public class UserController {
         String test = this.userService.handleHello();
         model.addAttribute("object1", test);
         model.addAttribute("object2", "Hello from controller");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getCreateUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "/admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUser(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println(user);
         return "hello";
     }
 }
