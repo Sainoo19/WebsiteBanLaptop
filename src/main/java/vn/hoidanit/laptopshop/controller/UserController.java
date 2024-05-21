8b761ef25b1ac152fdec9a1bffa600f468fcb709
Sainoo <nvtrung19.work@gmail.com>
2024-05-21 15:20:10 +0700

    CRUD User for Admin

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,8 +1,11 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,29 +15,47 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("object1", test);
+
+        model.addAttribute("object1", "test");
         model.addAttribute("object2", "Hello from controller");
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
+    @RequestMapping("/admin/user/create")
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "/admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUser(Model model, @ModelAttribute("newUser") User user) {
-        System.out.println(user);
-        return "hello";
+        this.userService.handleSaveUser(user);
+        return "redirect:/admin/user";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        List<User> lstUser = this.userService.getAllUsers();
+        System.out.println(lstUser);
+        model.addAttribute("users", lstUser);
+        return "/admin/user/user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getDetailUserPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "/admin/user/detail-user";
+    }
+
 }
